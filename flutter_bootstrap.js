573970df56c7195ab6688531ac93889067e1466f
Rilo Raspopo <rilo.raspopo37@gmail.com>
2026-04-27 15:30:31 +0700

    test

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "695131111" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4114229640" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
